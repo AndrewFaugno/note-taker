fc74d53619ffe627733fd8d11da4a91970734a2d
Andrew Faugno <andrewfaugno825@gmail.com>
2022-06-09 23:30:24 -0400

    created delete route for deleting tasks and updating data file with new arr

--- __tests__/notes.test.js
+++ __tests__/notes.test.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const createNewNote = require('../lib/notes');
+const { createNewNote, deleteNoteData } = require('../lib/notes');
 const { notes } = require('../db/db.json');
 jest.mock('fs');
 
@@ -12,3 +12,26 @@ test('creates new note object', () => {
     expect(newNote.title).toBe('testTitle');
     expect(newNote.text).toBe('testText');
 });
+
+test('deletes a note object', () => {
+    const noteArr = [
+        {
+            title: 'name',
+            text: 'text',
+            id: '1'
+        },
+        {
+            title: 'name',
+            text: 'text',
+            id: '2'
+        },
+        {
+            title: 'name',
+            text: 'text',
+            id: '3'
+        }
+    ];
+    const updatedArr = deleteNoteData(2, noteArr);
+
+    expect(updatedArr.length).toEqual(2)
+})
--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { notes } = require('../../db/db.json');
-const createNewNote = require('../../lib/notes');
+const {createNewNote, deleteNoteData} = require('../../lib/notes');
+const uniqid = require('uniqid');
 
 router.get('/notes', (req, res) => {
     res.json(notes);
@@ -8,10 +9,17 @@ router.get('/notes', (req, res) => {
 
 router.post('/notes', (req, res) => {
     // set id based on what next index of the array will be
-    req.body.id = notes.length.toString();
+    req.body.id = uniqid();
 
     const note = createNewNote(req.body, notes);
     res.json(note);
-})
+});
+
+router.delete('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const updatedNotes = deleteNoteData(id, notes);
+
+    res.send('DELETE note request recieved');
+});
 
 module.exports = router;
